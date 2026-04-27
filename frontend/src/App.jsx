0c61945e577b5486a408be4fdffe081e27f2192b
Deepak Kumar <deepakraj123g@gmail.com>
2026-04-28 00:08:37 +0530

    new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,7 +9,7 @@ import DestinationRoute from './pages/Destination'
 import TripDetails from './pages/TripsRoute'
 import AboutIndia from './pages/AboutIndia'
 import ContactRoute from './pages/ContactRoutte'
-import NotFound from './pages/NotFound.jsx'
+import Notfound from './pages/Notfound.jsx'
 import Login from './pages/auth/login'
 import SignUp from './pages/auth/signup'
 import { Toaster } from 'react-hot-toast'
@@ -50,7 +50,7 @@ const App = () => {
           <Route path='/contact' element={<UserLayout><ContactRoute/></UserLayout>} />
           
           <Route path='/admin/*' element={<AdminRoute />} />
-          <Route path='*' element={<NotFound />} />
+          <Route path='*' element={<Notfound />} />
       </Routes>
     </>
   )
