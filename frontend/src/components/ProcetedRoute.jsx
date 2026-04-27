6aaecd87cd0044f0e059a4f3d5a0286d912d0f3e
Deepak Kumar <deepakraj123g@gmail.com>
2026-04-28 00:18:23 +0530

    login changes

--- frontend/src/components/ProcetedRoute.jsx
+++ frontend/src/components/ProcetedRoute.jsx
@@ -1,9 +1,15 @@
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { useAuthStore } from '../store/useAuthStore';
 
 const ProtectedRoute = ({ children }) => {
   const user = useAuthStore((state) => state.user);
-  return user ? children : <Navigate to="/login" />;
+  const location = useLocation();
+
+  if (!user) {
+    return <Navigate to="/auth/login" state={{ from: location }} replace />;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
